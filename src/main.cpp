75e696e3aff92d880c3c46f4723e5685bd8b4fb9
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-03-21 02:58:00 -0700

    🎨 Moved consts into a header

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
+//
+// Created by Aubrey on 3/20/2022.
+//
+
+#include "main.hpp"
 #include <dpp/dpp.h>
 #include <toml.hpp>
 #include <iostream>
-
-const auto configFile = toml::parse("config/config.toml");
-const auto englishFile = toml::parse("config/en_us.toml");
-
-const auto token = toml::find<std::string>(configFile, "config", "token");
-const auto guildID = toml::find<dpp::snowflake>(configFile, "config", "guildID");
-
-const auto howdyDesc = toml::find<std::string>(englishFile, "description", "howdy");
-const auto howdyOutput = toml::find<std::string>(englishFile, "output", "howdy");
-
-const auto testModalDesc = toml::find<std::string>(englishFile, "description", "testModal");
-const auto testModalTitle = toml::find<std::string>(englishFile, "modals", "test_modal", "title");
-const auto testModalComponent1Label = toml::find<std::string>(englishFile, "modals", "test_modal", "component1", "label");
-const auto testModalComponent1Placeholder = toml::find<std::string>(englishFile, "modals", "test_modal", "component1", "placeholder");
-const auto testModalComponent2Label = toml::find<std::string>(englishFile, "modals", "test_modal", "component2", "label");
-const auto testModalComponent2Placeholder = toml::find<std::string>(englishFile, "modals", "test_modal", "component2", "placeholder");
-
 
 int main() {
     dpp::cluster bot(token);
