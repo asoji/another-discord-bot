a5a4b90371f88bd49d4e2a88c1abaff6dccb7e3f
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-03-20 17:55:56 -0700

    🔧 Switched stuff over to TOML + changed version

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <toml.hpp>
 #include <iostream>
 
-const auto configFile = toml::parse("config.toml");
+const auto configFile = toml::parse("config/config.toml");
+const auto englishFile = toml::parse("config/en_us.toml");
+
 const auto token = toml::find<std::string>(configFile, "config", "token");
 const auto guildID = toml::find<dpp::snowflake>(configFile, "config", "guildID");
+
+const auto howdyDesc = toml::find<std::string>(englishFile, "description", "howdy");
+const auto howdyOutput = toml::find<std::string>(englishFile, "output", "howdy");
 
 int main() {
     dpp::cluster bot(token);
@@ -13,13 +18,13 @@ int main() {
 
     bot.on_interaction_create([](const dpp::interaction_create_t& event){
         if (event.command.get_command_name() == "howdy") {
-            event.reply("Howdy nerd.");
+            event.reply(howdyOutput);
         }
     });
 
     bot.on_ready([&bot](const dpp::ready_t event){
         if (dpp::run_once<struct register_bot_commands>()) {
-            bot.guild_command_create(dpp::slashcommand("howdy", "Say Howdy to the bot!", bot.me.id), guildID);
+            bot.guild_command_create(dpp::slashcommand("howdy", howdyDesc, bot.me.id), guildID);
         }
     });
 
